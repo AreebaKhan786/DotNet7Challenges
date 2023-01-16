71be8d9e08ae935e169f97a381cf010d17bebb8a
Umair tahir <umairtahir.sparkers@gmail.com>
2023-01-17 00:07:30 +0500

    Add project files.

---
+++ DotNet7Challenges.Tests/SumTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet7Challenges.Tests;
+
+using NUnit.Framework;
+
+class ChallengesTests
+{
+    [Test]
+    public void TestPositiveAddTillNumber()
+    {
+        // Arrange
+        int n = 5;
+        int expectedSum = 15;
+        Challenges challenges = new Challenges();
+
+        // Act
+        int actualSum = challenges.AddTillNumber(n);
+
+        // Assert
+        Assert.AreEqual(expectedSum, actualSum);
+    }
+
+    [Test]
+    public void TestNegativeAddTillNumber()
+    {
+        // Arrange
+        int n = -5;
+        int expectedSum = -15;
+        Challenges challenges = new Challenges();
+
+        // Act
+        int actualSum = challenges.AddTillNumber(n);
+
+        // Assert
+        Assert.AreEqual(expectedSum, actualSum);
+    }
+
+    [Test]
+    public void TestZeroAddTillNumber()
+    {
+        // Arrange
+        int n = 0;
+        int expectedSum = 0;
+        Challenges challenges = new Challenges();
+
+        // Act
+        int actualSum = challenges.AddTillNumber(n);
+
+        // Assert
+        Assert.AreEqual(expectedSum, actualSum);
+    }
+}
---
+++ DotNet7Challenges/Challenges.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet7Challenges;
+
+public class Challenges
+{
+    /*
+    Challenge:
+    Write a program that takes an integer as input and returns the sum of all the integers from 1 to that input number.
+
+    Constraints:
+
+    The input number can be in the range of 1 to 10^6
+    Example:
+    Input: 5
+    Output: 15 (1+2+3+4+5 = 15)
+
+    This problem requires you to:
+
+    accept an input from the user
+    use a loop to iterate from 1 to the input number
+    perform an operation (addition) during each iteration
+    output the final result
+    You have to solve the problem using all the loops (for, while, do while, foreach) and recursion
+    */
+    public int AddTillNumber(int number)
+    {
+        return 0;
+    }
+}
